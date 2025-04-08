3e5dc043e2988a4c5277c5d17adbc242f1e5ba02
SPhilogene <95000285+SPhilogene@users.noreply.github.com>
2025-04-08 12:19:41 -0400

    Add files via upload

---
+++ GeorgiaPoints.js
@@ -0,0 +1,135 @@
+var GeorgiaPoints = [{
+    "type": "Feature",
+    "properties": {
+        "name": "SisterSong",
+        "show_on_map": true,
+        "image": "https://images.squarespace-cdn.com/content/v1/5a6d2e7e692ebebca8c59996/1613161701846-03R1AYA7NAIUZEU8HQFQ/Sistersong+Logo.png",
+	"about": "SisterSong, the largest Black Reproductive Justice collective, is a Southern based, national membership organization; our purpose is to build an effective network of individuals and organizations to improve institutional policies and systems that impact the reproductive lives of marginalized communities.",
+    "description": "",
+    "url":"https://www.sistersong.net/about-x2",
+    "address1":"P.O. Box 94408",
+    "address2":"Atlanta, GA 30377", 
+    "contact": "info@sistersong.net ",
+    "iconUrl": `sankofa-small.png`,
+	"service": "#58D2D7ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.1086083288352, 33.7158300202295],
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Patchwork City Farm",
+        "show_on_map": true,
+        "image": "https://images.squarespace-cdn.com/content/v1/59b2ec53f14aa14c97a217b2/b00cb917-f757-4e14-9848-439d170f6edf/pcf_fulllogo_LB.jpg",
+        "about": "Jamila Norman, a.k.a (Farmer J) is a first generation daughter to Caribbean parents whose history is rooted in agriculture. She is an internationally recognized urban farmer, food activist and mother based in Atlanta, GA.  She is a University of Georgia graduate with a degree in Environmental Engineering. After 10 years in her professional career, Jamila has now committed fully to operating her independent, organic urban farm, Patchwork City Farms, which she founded 2010.  In 2014 she served as a US delegate to Slow Food’s Terra Madre Salone del Gusto, in Turin, Italy.  She and her farm have been featured in various publications, including Modern Farmer Magazine, SeedStock.com, The Library of Congress and the Southern Foodways Alliance oral history project.  Jamila is also one of the founding members and current manager of the South West Atlanta Growers Cooperative (SWAG Coop), formed in 2010 as a cooperative centered around black urban farmers in Atlanta’s burgeoning urban agriculture movement.  SWAG Cooperative exists to strengthen Atlanta’s Black farmers and to create a transformative, environmentally and culturally responsible Atlanta food system that contributes to a robust and equitable high quality of life for its farmers and communities.  Most recently, Jamila has joined the board of Georgia Organics, a member supported, non-profit organization connecting organic food from Georgia farms to Georgia families, and is an active member of the Atlanta Farmers Coalition. Jamila is recognized as a Cherry Bombe 100, which is list that celebrates women in food who inspire us every day with their creativity, energy, humanity, and hard work",
+	"description": "",
+    "url":"https://www.patchworkcityfarms.com/",
+    "address1":"902 Pinehurst Terrace SW⁠",
+    "address2":"Atlanta, GA 30310⁠", 
+    "contact": "farmerj@patchworkcityfarms.com",
+    "iconUrl": `sankofa-small.png`,
+	"service": "#C2EDE8ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.4310115480072, 33.7307258116651]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Feminist Women Health Center",
+        "show_on_map": true,
+        "image": "https://feministcenter.org/wp-content/uploads/2019/10/FWHC-horizontal-logo_purpleonwhite_small.png",
+	"about": "Providing safe, affordable and compassionate abortion and reproductive healthcare to all who need it without judgment. Your Abortion Clinic in Atlanta, GA.",
+    "description": "",
+    "url":"https://feministcenter.org",
+    "address1":"1924 Cliff Valley Way NE",
+    "address2":"Atlanta GA, 30329", 
+    "contact": "https://feministcenter.org/newsletter/",
+    "iconUrl": `sankofa-small.png`,
+	"service": "#1A8B91ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.3244720821699, 33.8359005379645]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Center for Black Women's Wellness",
+        "show_on_map": true,
+        "image": "https://www.cbww.org/wp-content/uploads/2021/08/cropped-CBWW_Logo_SM_Small-Trans.png",
+	"about": "We provide care to those without health insurance on a sliding fee scale. CBWW is here to help you take charge of your health by providing affordable healthcare and health programs to support your health journey. Our healthcare and health programs include: Women’s Health, Primary Care, Mental & Emotional Wellbeing, Health Education and promotion, and Breast and Cervical Cancer programs.",
+        "description": "",
+    "url":"https://www.cbww.org/",
+    "address1":"477 Windsor St SW, Suite 309",
+    "address2":"Atlanta, GA, 30312", 
+    "contact": "info@cbww.org",
+    "iconUrl": `nkyinkyim-small.png`,
+	"service": "#1A8B91ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.3997961441763, 33.7413456795082]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Dr. Karenne Fru",
+        "show_on_map": true,
+        "image": "https://static.wixstatic.com/media/5e8b08_f2b5b02875314f4582ba3cb849ab0ed6~mv2.jpg/v1/fill/w_640,h_902,al_t,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/5e8b08_f2b5b02875314f4582ba3cb849ab0ed6~mv2.jpg",
+	"about": "Dr. Karenne Fru is double board-certified in Obstetrics and Gynecology and Reproductive Endocrinology and Infertility. She is based in Atlanta, GA, with over 10 years of experience in the field. She is dedicated to providing personalized and compassionate care to her patients and helping them achieve their reproductive goals.",
+        "description": "These services encompass fertility testing and diagnosis, ovulation induction, in vitro fertilization (IVF), egg freezing, and other related options.",
+	"url":"https://www.drkarennefru.com/",
+    "address1":"5887 Glenridge Drive, Ste. 300",
+    "address2":"Atlanta, GA 30328", 
+    "contact": "info@munafertility.com",
+    "iconUrl": `nea-onnim-small.png`,
+    "service": "#BAF3F0ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.3600591576705, 33.9160865298075]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Reaching Our Sisters Everywhere - ROSE",
+        "show_on_map": true,
+        "image": "https://breastfeedingrose.org/wp-content/uploads/2021/05/ROSE_300dpi-vector-web-1-768x323-1.png",
+	"about": "ROSE is a national nonprofit organization with a mission to serve communities, through a continued focus on centering the breastfeeding needs of Black families and impacting broader maternal, infant, and family health outcomes across the nation. Reaching Our Sisters Everywhere (ROSE) Inc. was founded to address breastfeeding disparities for communities of color. ROSE works to normalize breastfeeding by providing resources and networking opportunities for individuals and communities. As a national expert, and in partnership with communities, we build equity in maternal and child health through culturally competent training, education, advocacy, and support.",
+        "description": "",
+	"url":"https://breastfeedingrose.org",
+    "address1":"3035 Stone Mountain St. #1076",
+    "address2":"Lithonia, GA 30058", 
+    "contact": "https://breastfeedingrose.org/contact/",
+    "iconUrl": `funtumfunefu-denkyemfunefu-small.png`,
+    "service": "#58D2D7ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.1086083288352, 33.7158300202295]
+    }
+}, {
+    "type": "Feature",
+    "properties": {
+        "name": "Tamara Akua Hunter",
+        "show_on_map": true,
+        "image": "https://custom-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,fl_lossy,h_9000,w_1200,f_auto,q_auto/2228331/359705_353114.jpg",
+        "about": "Tamara Hunter is a Licensed Professional Counselor and Dance/Movement Therapist. She earned a M.S. degree in Dance/Movement Therapy from Pratt Institute, M.A. degree in School Counseling and B.A. degree in Early Childhood Education from Clark Atlanta University. Tamara currently provides therapy in a private practice setting located Atlanta, Georgia. She is a perinatal trained provider specializing in infertility, pregnancy, reproductive and infant loss, still birth, birth trauma, and postpartum mood and anxiety disorders. Tamara is passionate about empowering women by imparting support, encouragement, education & therapeutic coping techniques.",
+	"description": "Perinatal Mental Health Services",
+	"url":"https://www.tamaraakuahunter.com/",
+    "address1":"",
+    "address2":"", 
+    "contact": "tamaraakuahunter@gmail.com",
+    "iconUrl": `adinkrahene-small.png`,
+    "service": "#BAF3F0ff"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-84.1086083288352, 33.7158300202295]
+    }
+}];
+
